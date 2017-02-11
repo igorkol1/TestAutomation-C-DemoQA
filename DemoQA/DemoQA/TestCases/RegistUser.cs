c0cb06cd24a842a35a5cc9439a930dc19a54a253
Igor <Igor@IGOR-YOGA>
2017-02-11 10:59:30 +0100

    First Configuration, Folder orgenization

---
+++ DemoQA/DemoQA/TestCases/RegistUser.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoQA.TestCases
+{
+    class RegistUser
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new FirefoxDriver();
+        }
+
+        [Test]
+        public void NavigateToDemoQA()
+        {
+            driver.Navigate().GoToUrl("http://demoqa.com/");
+        } 
+
+    }
+}
